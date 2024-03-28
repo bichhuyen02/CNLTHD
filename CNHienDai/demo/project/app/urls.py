502b4d8c02cbf7d8d23aec038f3c12675d777a0e
bichhuyen2109 <tsanthibichhuyen251@gmail.com>
2024-03-28 11:20:49 +0700

    de

--- CNHienDai/demo/project/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('courses/', views.index, name="index"),
-    path('courses/<int:course_id>', views.list, name="list"),
-    path('categorise/', views.index, name="index")
-]
